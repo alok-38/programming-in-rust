83574687725d864f0278949cec747ebf2a31fb2e
ThePrimeagen <the.primeagen@aol.com>
2026-06-27 07:00:23 +0530

    Working with variables

--- variables.rs
+++ variables.rs
@@ -1,7 +1,6 @@
 fn main() {
-    const X: f64 = 5.0;
-    const Y: f64 = 3.14;
-    const Z: f64 = X * Y;
-
-    println!("{} * {} = {}", X, Y, Z);
+    let mut x: i32 = 5;
+    println!("x is {}", x);
+    x = 15;
+    println!("x is {}", x);
 }
