0281f3b1ca860d75c7bd4834be07fe17fd9680b6
ThePrimeagen <the.primeagen@aol.com>
2026-07-01 07:48:02 +0530

    I'm a rustacean

--- printing.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("Hello from  Rust");
-    let x = 42;
-    println!("My lucky number is {}.", x);
-    let xs = vec![1, 2 ,3];
-    println!("The list is: {:?}", xs);
-    println!("This is information");
-    eprintln!("This is an error! :(");
-}
--- variables.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let mut x: i32 = 5;
-    println!("x is {}", x);
-    x = 15;
-    println!("x is {}", x);
-}
